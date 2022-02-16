66c8d14bd02b2835c09be190c86d1bfe81362196
Voice <git497075391>
2022-02-16 11:58:12 -0500

    changes

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -4,29 +4,34 @@
     for (let row = 1; row <= 9; row++) {
         let rowElement = $('<tr>');
         let data = $('<td>');
-        for (let col = 1; col <= 9; col++) {
+        for (let col = 0; col <= 8; col++) {
             let data = $('<td>');
             data.text(''+col+row);
-            data.attr('id', '' + col + row)
+            data.attr('id', '' + row + col);
             //alert(data.attr('id'))
             board.append(data);
             
         }
         board.append(rowElement);
-        console.log(row)
+        console.log(row);
     }
 
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 0; i <= 8; i++) {
         let data = $('<td>');
-        if (i != 9) {
-            data.click(interfaceOnClick)
+        if (i != 8) {
+            data.click(interfaceOnClick);
             data.text(i);
-
+            interface.append(data);
+        }
+        else {
+            interface.append('<td><a href="#"><img src="images/undo.png" alt="undo" /></a></td>');
         }
         
-        interface.append(data);
+        
     }
+    var selected = '';
     function interfaceOnClick() {
-        alert($(this).text())
+        alert($(this).text()+'is clicked');
+        selected = $(this).text();
     }
 })
